5ab3dabc0b2567aef59cb243ec1e96c95899d5c7
Ivan-Stefashin <119510323+Ivan-Stefashin@users.noreply.github.com>
2023-06-05 12:47:12 +0300

    v2
    2

--- My project/Assets/scripts/NewBehaviourScript.cs
+++ My project/Assets/scripts/NewBehaviourScript.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class sprite : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
+
+    private Transform trans;
+    public GameObject obj;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyUp (KeyCode.D)) 
+        {
+            obj.Position.x = ;
+        }
         
     }
 }
