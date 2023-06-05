a16f58d3a6b18aedcdb3747fb53197c338e21fe5
Ivan-Stefashin <119510323+Ivan-Stefashin@users.noreply.github.com>
2023-06-05 20:58:55 +0300

    sprites for menu

--- My project/Assets/scripts/NewBehaviourScript.cs
+++ My project/Assets/scripts/NewBehaviourScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-
+    public float speed = 5f;
     private Transform trans;
     public GameObject obj;
 
@@ -12,16 +12,18 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        trans = GetComponent<Transform>();
+       // trans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp (KeyCode.D)) 
-        {
-            obj.Position.x = ;
-        }
-        
+        /* if (Input.GetKeyUp (KeyCode.D)) 
+         {
+             obj.Position.x = ;
+         }
+         */
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
     }
 }
