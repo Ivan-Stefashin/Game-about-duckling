fe8931f03c4d3bfc33985342b3b31da94be4cf86
Ivan-Stefashin <119510323+Ivan-Stefashin@users.noreply.github.com>
2023-06-06 16:53:41 +0300

    Движение (Move), Переключение слоёв (Layer)
    Scripts
    1 Move
    2 Layer

---
+++ My project/Assets/scripts/MovePlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    public float speed;
+    private Vector2 derection;
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb  = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        derection.x = Input.GetAxisRaw("Horizontal");
+        derection.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + derection * speed * Time.fixedDeltaTime);
+        
+    }
+}
